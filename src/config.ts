9e799d37b0f6bc3f01efd10a3319dda925a84528
qiyu666 <3270581950@qq.com>
2026-03-04 21:35:29 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -62,17 +62,24 @@ export const profileConfig: ProfileConfig = {
 			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://blog.qiyu666.dpdns.org",
 		},
 		{
 			name: "Steam",
 			icon: "fa6-brands:steam",
 			url: "https://store.steampowered.com",
 		},
+  {
+			name: "Alist",
+			icon: "fa6-bran:alist",
+			url: "https://alist.qiyu666.dpdns.org",
+		},
+	],
+};
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/qiyu666",
 		},
 	],
 };
